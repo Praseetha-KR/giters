dfd8e60908cda9f96196c296e849c06acbddd12b
Praseetha-KR <voidimagineer@yahoo.com>
2016-07-30 14:46:24 +0530

    Test for decimal part

--- src/tests/unit/test_kformat.js
+++ src/tests/unit/test_kformat.js
@@ -10,9 +10,9 @@ describe('kFormat component', function() {
         $filter = _$filter_;
     }));
     it('should format to K unit for numbers > 10000', function() {
-        var num = 23456, result,
+        var num = 10001, result,
         result = $filter('kFormat')(num, 'kFormat');
-        result.should.containEql('23.5K');
+        result.should.containEql('10.0K');
     });
     it('should format to K unit for number = 10000', function() {
         var num = 10000, result,
@@ -20,8 +20,18 @@ describe('kFormat component', function() {
         result.should.containEql('10.0K');
     });
     it('should not format to K unit for numbers < 10000', function() {
-        var num = 567, result,
+        var num = 9999, result,
         result = $filter('kFormat')(num, 'kFormat');
-        result.should.containEql(567);
+        result.should.containEql(9999);
+    });
+    it('should floor succeeding decimal digit >= 5', function() {
+        var num = 12354, result,
+        result = $filter('kFormat')(num, 'kFormat');
+        result.should.containEql('12.4K');
+    });
+    it('should floor succeeding decimal digit < 5', function() {
+        var num = 12345, result,
+        result = $filter('kFormat')(num, 'kFormat');
+        result.should.containEql('12.3K');
     });
 })
